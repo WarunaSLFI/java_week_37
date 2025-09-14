0c4156736e8488fb735fd1f72ec630704c234e3b
WarunaSLFI <warunaslfi@gmail.com>
2025-09-15 00:03:57 +0300

    Added e07 App.java

---
+++ e07/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) {
+        // 1. Declare a constant
+        final int MAX_SCORE = 100;
+
+        // 2. Declare a variable less than MAX_SCORE
+        int score = 75;
+
+        // 3. Print both values
+        System.out.println("MAX_SCORE: " + MAX_SCORE);
+        System.out.println("score: " + score);
+    }
+}

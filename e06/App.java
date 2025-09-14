0c9723ee423ba918dc7ddd9b3268f9ebd8606828
WarunaSLFI <warunaslfi@gmail.com>
2025-09-15 00:03:16 +0300

    Added e06 App.java

---
+++ e06/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) {
+        // 1. Declare boolean variables
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+
+        // 2. Perform and print logical operations
+        System.out.println("isJavaFun AND isFishTasty: " + (isJavaFun && isFishTasty));
+        System.out.println("isJavaFun OR isFishTasty: " + (isJavaFun || isFishTasty));
+        System.out.println("NOT isJavaFun: " + (!isJavaFun));
+        System.out.println("NOT isFishTasty: " + (!isFishTasty));
+    }
+}

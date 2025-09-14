e70c00272369b9861deb9c024b9ab94b8d493242
WarunaSLFI <warunaslfi@gmail.com>
2025-09-14 23:58:56 +0300

    Added e02 App.java

---
+++ e02/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) {
+        
+        int age = 25;                 
+        double temperature = 36.6;    
+        char initial = 'W';            
+        
+        System.out.println("Age: " + age);
+        System.out.println("Temperature: " + temperature);
+        System.out.println("Initial: " + initial);
+    }
+}

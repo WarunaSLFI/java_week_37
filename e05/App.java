eb66bb373309e07dd1a66b7dd0b6d42c954ce8be
WarunaSLFI <warunaslfi@gmail.com>
2025-09-15 00:02:40 +0300

    Added e05 App.java

---
+++ e05/App.java
@@ -0,0 +1,53 @@
+public class App {
+    public static void main(final String[] args) {
+        // Number = -1
+        int number = -1;
+        System.out.println("number = " + number);
+        System.out.println("number > 0: " + (number > 0));
+        System.out.println("number < 10: " + (number < 10));
+        System.out.println("number > 0 && number < 10: " + ((number > 0) && (number < 10)));
+        System.out.println("!(number > 0 && number < 10): " + !((number > 0) && (number < 10)));
+        System.out.println("number > 0 || number < 10: " + ((number > 0) || (number < 10)));
+        System.out.println("-----------------------------");
+
+        // Number = 0
+        number = 0;
+        System.out.println("number = " + number);
+        System.out.println("number > 0: " + (number > 0));
+        System.out.println("number < 10: " + (number < 10));
+        System.out.println("number > 0 && number < 10: " + ((number > 0) && (number < 10)));
+        System.out.println("!(number > 0 && number < 10): " + !((number > 0) && (number < 10)));
+        System.out.println("number > 0 || number < 10: " + ((number > 0) || (number < 10)));
+        System.out.println("-----------------------------");
+
+        // Number = 1
+        number = 1;
+        System.out.println("number = " + number);
+        System.out.println("number > 0: " + (number > 0));
+        System.out.println("number < 10: " + (number < 10));
+        System.out.println("number > 0 && number < 10: " + ((number > 0) && (number < 10)));
+        System.out.println("!(number > 0 && number < 10): " + !((number > 0) && (number < 10)));
+        System.out.println("number > 0 || number < 10: " + ((number > 0) || (number < 10)));
+        System.out.println("-----------------------------");
+
+        // Number = 9
+        number = 9;
+        System.out.println("number = " + number);
+        System.out.println("number > 0: " + (number > 0));
+        System.out.println("number < 10: " + (number < 10));
+        System.out.println("number > 0 && number < 10: " + ((number > 0) && (number < 10)));
+        System.out.println("!(number > 0 && number < 10): " + !((number > 0) && (number < 10)));
+        System.out.println("number > 0 || number < 10: " + ((number > 0) || (number < 10)));
+        System.out.println("-----------------------------");
+
+        // Number = 10
+        number = 10;
+        System.out.println("number = " + number);
+        System.out.println("number > 0: " + (number > 0));
+        System.out.println("number < 10: " + (number < 10));
+        System.out.println("number > 0 && number < 10: " + ((number > 0) && (number < 10)));
+        System.out.println("!(number > 0 && number < 10): " + !((number > 0) && (number < 10)));
+        System.out.println("number > 0 || number < 10: " + ((number > 0) || (number < 10)));
+        System.out.println("-----------------------------");
+    }
+}

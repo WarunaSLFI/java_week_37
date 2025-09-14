1cd3fb4992a043b065bedef7e104999b0747836f
WarunaSLFI <warunaslfi@gmail.com>
2025-09-15 00:00:06 +0300

    Added e03 App.java

---
+++ e03/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) {
+        
+        int a = 15;
+        int b = 4;  
+
+        
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int quotient = a / b;   
+        int modulo = a % b;    
+
+       
+        System.out.println("a = " + a + ", b = " + b);
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Modulo: " + modulo);
+    }
+}

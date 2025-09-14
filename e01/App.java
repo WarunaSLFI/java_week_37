e5896d0483fcb0bb881a71317f566bef5d9c3112
WarunaSLFI <warunaslfi@gmail.com>
2025-09-14 23:56:22 +0300

    Added e01 App.java

---
+++ e01/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args){
+        int x = 10;
+        float y = 5.5f;
+        boolean flag = true;
+        char letter = 'c';
+        short year = 2002;
+        long population = 7000000000l;
+        byte num = 100;
+        double pi = 3.13457599923384753929348;
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(flag);
+        System.out.println(letter);
+        System.out.println(year);
+        System.out.println(population);
+        System.out.println(num);
+        System.out.println(pi);
+    }
+}
